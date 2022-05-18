ed1dae2804936db41bad88ff163eb39dc45612db
Ivan Vasquez <ivanjv1234@gmail.com>
2022-05-18 01:43:11 -0500

    feat: add User resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username:'Ivan',password:'password')
+User.create(username:'Sergio',password:'password')
+User.create(username:'Norma',password:'password')
+User.create(username:'Alberto',password:'password')
+User.create(username:'Marta',password:'password')
