97b39c5317ac269de921cedcf14be3cbb2fc398b
Ivan Vasquez <ivanjv1234@gmail.com>
2022-05-21 01:00:51 -0500

    Add flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,11 @@ import "channels"
 
 document.addEventListener('turbolinks:load', () => {
     $('.ui.dropdown').dropdown()
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+      });
 })
+
 
 Rails.start()
 Turbolinks.start()
