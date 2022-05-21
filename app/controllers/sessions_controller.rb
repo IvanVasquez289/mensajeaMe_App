8d11a51e6a5ddd7667b46fcd475e4db0c10221a0
Ivan Vasquez <ivanjv1234@gmail.com>
2022-05-21 00:34:31 -0500

    feat: Add authentication system - create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Iniciado sesion con exito'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Hubo un error con tus credenciales'
+      render 'new'
+    end
   end
 
+  # cuando cierras sesion se destruye la sesion
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Se ha cerrado la sesión"
+    redirect_to login_path
+  end
 end 
