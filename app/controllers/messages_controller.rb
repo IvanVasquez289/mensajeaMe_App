ebf3cd6fe3a205c417b89ee7ef7056cf06dc17da
Ivan Vasquez <ivanjv1234@gmail.com>
2022-05-21 17:23:34 -0500

    feat: Add messages from ui and create messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  before_action :require_user
+  def create
+    @message = Message.new(message_params)
+ 
+    @message.user = current_user
+    if @message.save
+      redirect_to root_path
+    else 
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
