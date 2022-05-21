2af865900c7c3d63523d0c89a9513f7bcaba3304
Ivan Vasquez <ivanjv1234@gmail.com>
2022-05-21 01:21:05 -0500

    feat: Restrict views at controller level

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,7 @@
 class ChatroomController < ApplicationController
-    def index
-      @messages = Message.all
-    end
+  before_action :require_user
+  
+  def index
+    @messages = Message.all
+  end
 end 
